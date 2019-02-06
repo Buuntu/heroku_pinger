c6f8735132953b34f67657306aa73d212ef1f424
Gabriel Abud <gabriel.jabud@gmail.com>
2019-02-06 09:57:20 -0800

    First commit

---
+++ clock.py
@@ -0,0 +1,25 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+import os
+import requests
+import sys
+
+sched = BlockingScheduler()
+
+if(not 'PING_HOST' in os.environ):
+    sys.exit('PING_HOST environment variable is not set')
+
+hostname = os.environ['PING_HOST']
+
+@sched.scheduled_job('interval', minutes=28)
+def timed_job():
+    response = requests.get(hostname)
+
+    #and then check the response...
+    if response.status_code == 200:
+        print(hostname, 'is up!')
+    else:
+        print(hostname, 'is down!')
+    print('This job is run every three minutes.')
+
+sched.start()
+
